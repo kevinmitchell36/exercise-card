0babd28b626ede087af31d953d9d0d5e5a96d683
kevinmitchell36 <kevingm31@gmail.com>
2023-07-23 18:50:41 -0500

    Create new exercise element
    Creates a new exercise element with just a name

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+// Form
+const nameField = document.getElementById("movement");
+const list = document.getElementById("all-exercises");
+const form = document.getElementById("form");
+
+function addItem(e) {
+  e.preventDefault();
+  const exerciseName = nameField.value
+
+  if (exerciseName === '') {
+    alert('Please add to the name field');
+    return;
+  }
+
+  const div = createCard("card", exerciseName);  
+  list.appendChild(div)
+
+}
+
+function createCard(className, exerciseName) {
+  const div = document.createElement('div');
+  div.className = className;
+  const ul = createUl('exercise', exerciseName);
+  div.appendChild(ul);
+  return div
+}
+
+function createUl(className, exerciseName) {
+  const ul = document.createElement('ul');
+  ul.className = className;
+  const li = createLi(exerciseName);
+  ul.appendChild(li)
+  return ul;
+}
+
+function createLi(exerciseName) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(exerciseName));
+  return li;
+}
+
+form.addEventListener('submit', addItem);
+
+
+
+
+// Modal
 const modal = document.getElementById("form");
-
 const btn = document.getElementById("add-item");
-
 const closers = document.querySelectorAll("button#close, i#close");
-
 
 
 btn.onclick = function() {
